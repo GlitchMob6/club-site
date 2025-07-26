fa12c5690eab717912f15358a0f17cb8be1fca67
GlitchMob6 <pushkarchawhan@gmail.com>
2025-07-26 15:30:54 +0530

    changes log 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from'@astrojs/sitemap';
-
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-	integration: [mdx(), sitemap()],
-	output: 'static'
-})
+  site: 'https://club-site-nine.vercel.app',  // or your real Vercel URL
+  integrations: [mdx(), sitemap()],
+});
+
